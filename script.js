a6f90dbbb33a193fa79afaafa80118563f03bac3
Dustin Hoffman <dwhoffman@hotmail.com>
2014-06-23 20:28:48 -0400

    Finished intro to directives.

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-(function() {
+(function () {
 
     var app = angular.module("githubViewer", []);
 
-    var MainController = function($scope, $http) {
+    var MainController = function ($scope, $http) {
 
-        var onUserComplete = function(response) {
+        var onUserComplete = function (response) {
             $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                 .then(onRepos, onError);
         };
 
-        var onError = function(reason) {
-            $scope.error = "Could not fetch the user";
+        var onRepos = function (response) {
+            $scope.repos = response.data;
         };
 
-        $scope.search = function(username) {
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data.";
+        };
+
+        $scope.search = function (username) {
             $http.get("https://api.github.com/users/" + username)
                 .then(onUserComplete, onError);
         };
 
         $scope.username = "angular";
         $scope.message = "GitHub Viewer";
-
+        $scope.repoSortOrder = "-stargazers_count";
     };
 
     app.controller("MainController", ["$scope", "$http", MainController]);
 
-}());
+} ());
